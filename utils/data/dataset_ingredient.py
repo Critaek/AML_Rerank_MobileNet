50703089aeaf3c2b7f6f2249fae5c3f9d6fd5720
Critaek <serra.cri2000@gmail.com>
2023-08-30 17:10:21 +0200

    Commit

--- utils/data/dataset_ingredient.py
+++ utils/data/dataset_ingredient.py
@@ -143,8 +143,8 @@ def get_sets(name, data_path, train_folder, test_folder, num_workers, M=10, alph
     queries_folder = os.path.join(test_folder, queries_folder_name)
 
     base_transform = transforms.Compose([
-            transforms.Resize(512),
             transforms.ToTensor(),
+            transforms.Resize((512,512)),
             transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
         ])
     
