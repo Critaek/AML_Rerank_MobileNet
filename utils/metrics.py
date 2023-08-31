555c08bcac8ab6b6faa36082e16e9123a8a55280
Critaek <serra.cri2000@gmail.com>
2023-08-31 21:26:37 +0200

    Commit

--- utils/metrics.py
+++ utils/metrics.py
@@ -269,7 +269,7 @@ def recall_at_ks_rerank(
 
     for query_index, preds in enumerate(predictions):
         for i, n in enumerate(ks): #1, 5, 10, 20
-            if np.any(np.in1d(preds[:n], cache_nn_inds[query_index][:n])):
+            if np.any(np.in1d(preds[:n], ground_truth[query_index])):
                 recalls[i:] += 1
                 break
     
