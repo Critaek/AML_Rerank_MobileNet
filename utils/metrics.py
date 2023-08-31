8a080c0f949699e39a3b3436980ff0a1063cd33d
Critaek <serra.cri2000@gmail.com>
2023-08-31 17:28:55 +0200

    Commit

--- utils/metrics.py
+++ utils/metrics.py
@@ -220,7 +220,9 @@ def recall_at_ks_rerank(
         scores.append(k_scores)
     """
     print('time', total_time/num_samples)
+    print(f"scores len before stack: {len(scores)}")
     scores = torch.stack(scores, -1)
+    print(f"scores size after stack: {scores.size()}")
     closest_dists, indices = torch.sort(scores, dim=-1, descending=True)
     closest_dists = closest_dists.numpy()
     print(f"indices shape: {indices.shape}")
