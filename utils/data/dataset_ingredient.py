8822a771b7f88452f995e17891fd958fe8bc9805
Critaek <serra.cri2000@gmail.com>
2023-08-31 21:02:18 +0200

    Commit

--- utils/data/dataset_ingredient.py
+++ utils/data/dataset_ingredient.py
@@ -196,7 +196,6 @@ def get_sets(name, data_path, train_folder, test_folder, num_workers, M=10, alph
     print(f"distances: {distances[0][0:8]}")
     print(f"positives: {positives_per_query[0][0:8]}")
     print(f"image_path of closest: {samples_database[positives_per_query[0][0]][0]}")
-    print(f"image_path of closest predicted: {samples_database[1932][0]}")
     
     with open("/content/AML_Rerank_MobileNet/rrt_sop_caches/rrt_r50_sop_nn_inds_positives.pkl", "wb+") as f:
         pickle.dump(positives_per_query, f)
