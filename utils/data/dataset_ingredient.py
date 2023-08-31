255c9e7357da3ac4ea19185f63c3dc4015bcffe4
Critaek <serra.cri2000@gmail.com>
2023-08-31 02:06:21 +0200

    commit

--- utils/data/dataset_ingredient.py
+++ utils/data/dataset_ingredient.py
@@ -29,7 +29,7 @@ def config():
     batch_size = 32
     sample_per_id = 2
     assert (batch_size % sample_per_id == 0)
-    test_batch_size = 1024
+    test_batch_size = 256
     sampler = 'triplet' #"random"
 
     num_workers = 8 
